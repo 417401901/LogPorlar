dcb6b4901060940b5cb7e5a0f3e4f753e47c012f
417401901 <wx1998@hotmail.com>
2018-05-31 14:04:51 +0800

    first commit

---
+++ FileConversion.cpp
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "opencv2/opencv.hpp"
+#include "iostream"
+
+using namespace std;
+using namespace cv;
+/******************************************************
+ * Copyright(C), 2018, SLDX Ambition
+ * 本程序用于全息LED灯的格式转换 .jpg .png .bmp -> .bin
+ * 程序基于opencv2.7版本库编写
+ * 作者：wangx
+ * 版本：1.0.0 2018.5.31 初步完成对于图像的极坐标转换
+ * 
+ ******************************************************/
+
+int main()
+{
+
+    return 0;
+}
